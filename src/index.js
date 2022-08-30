3a81ce077926aaf4e2457eca409f42ad25556350
AntonioRnk <rynkov.anton.viktorovich@gmail.com>
2022-08-30 14:28:04 +0300

    change-routes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    // only for github-pages
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
 );
